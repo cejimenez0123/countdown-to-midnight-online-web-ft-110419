e994d5d5a686f3ad869a3c5fdcfd97e384d99319
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-05 20:48:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(number)
+def countdown
 until number == 0
   puts "#{number} SECOND(S)!"
   number -=1
 end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
